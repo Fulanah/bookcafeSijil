d7e029359429add79f689d817d7d314b55d30ec2
Balqis Abd Manan <alarva36@yahoo.com>
2021-04-16 13:53:28 +0800

    Delete tooltip.js

--- tooltip.js
+++
@@ -1,7 +0,0 @@
-$(function () {
-    $('[data-toggle="tooltip"]').tooltip()
-  })
-
-  <button type="button" class="btn btn-secondary" data-toggle="tooltip" data-placement="top" title="Tooltip on top">
-  Tooltip on top
-</button>
